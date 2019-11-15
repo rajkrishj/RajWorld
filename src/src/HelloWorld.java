23a01eeef5ceaaf9154ee6ffe2d2aa40388c0637
rajkrishj <45170642+rajkrishj@users.noreply.github.com>
2019-11-15 14:36:07 +0100

    Update HelloWorld.java

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		new HelloWorld().sayHello();
-	}
+	
 	
 	public String sayHello(){
 		return "Hello Capco - I have done some changes";
